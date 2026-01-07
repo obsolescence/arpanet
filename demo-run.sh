89ed0393c330ad911bfabf3a64eaf6a2f561fa7a
obsolescence <oscar.vermeulen@hotmail.com>
2026-01-07 15:11:30 +0100

    add demo-run

---
+++ demo-run.sh
@@ -0,0 +1,45 @@
+echo "Example script to explain the setup"
+echo
+
+echo "First, start the web terminal server. It can run on the VPS but here, we start it only for local use:"
+echo
+echo "cd web-terminals/simh-server"
+cd web-terminals/simh-server
+
+echo "screen -dmS web-server ./start.sh local"
+screen -dmS web-server ./start.sh local
+
+cd ../..
+echo
+
+read -r -p "Press Enter to continue..."
+
+echo
+echo "now, start the web terminal client that lets the HTML page get its terminal data from the server:"
+echo
+echo "cd web-terminals/terminal-client"
+cd web-terminals/terminal-client
+
+echo "screen -dmS web-client ./start.sh"
+screen -dmS web-client ./start.sh
+
+cd ../..
+echo
+
+read -r -p "Press Enter to continue..."
+
+echo
+echo "Next, start up the arpanet so the web terminal has a network to connect to"
+cd mini
+./arpanet start
+cd ..
+echo
+echo "Done. Do screen -ls to inspect everything."
+echo
+echo "To stop the arpanet, do"
+echo "./mini/arpanet stop"
+echo
+echo "To stop the web client/server, "
+echo " screen -r web-client, and Ctrl-C"
+echo " screen -r web-server, and Ctrl-C"
+
