53042eb99ff7b266159a4c178ee30975d03c06fc
obsolescence <oscar.vermeulen@hotmail.com>
2026-01-07 19:42:40 +0100

    stop script is nice

---
+++ stop-demo.sh
@@ -0,0 +1,16 @@
+cd mini
+./arpanet stop
+cd ..
+
+screen -S web-client -X stuff $'\003'
+screen -S web-server -X stuff $'\003'
+
+echo "Stopped everything. To check, screen -ls should show no screen sessions:"
+sleep 1
+echo "screen -ls"
+screen -ls
+echo
+echo "Done."
+
+
+
