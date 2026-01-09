d69bd2a1ec07dd72c7773befbbb82e23d631b286
obsolescence <oscar.vermeulen@hotmail.com>
2026-01-09 19:12:26 +0100

    add waitsconnect to stop-demo

--- stop-demo.sh
+++ stop-demo.sh
@@ -4,6 +4,7 @@ cd ..
 
 screen -S web-client -X stuff $'\003'
 screen -S web-server -X stuff $'\003'
+screen -S waitsconnect -X stuff $'\003'
 
 echo "Stopped everything. To check, screen -ls should show no screen sessions:"
 sleep 1
